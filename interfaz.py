8fdc2bde84f15557faf2ac597eb92db1acd48d8f
Keryn <kartaviamiranda@gmail.com>
2020-11-17 17:43:55 -0600

    interfaz

--- interfaz.py
+++ interfaz.py
@@ -1,60 +1,80 @@
 from tkinter import *
 
+#Configuracion de ventana
 window = Tk()
-
 window.title("Proyecto")
+window.geometry('800x700') #Tamaño de ventana (x, y)
+window.resizable(0,0)
+window.configure(bg='white')
 
-lbl = Label(window, text="Sistema de Televigilancia Remota", font=("Arial Bold", 30)) #Título de ventana
+#Imagen en ventana
+path_imagen = '/home/gaby/Descargas/Proyecto2_Embebidos/camera1.png'
+img = PhotoImage(file = path_imagen)
 
-lbl.grid(column=0, row=0)
+#Etiqueta de ventana
+lbl = Label(window, text="Sistema de Televigilancia Remota", font=("Arial Bold", 25), bg='white') #etiqueta
+lbl.pack() #posicion de etiqueta
 
-window.geometry('850x850') #Tamaño de ventana
+imagen1 = Label(window, image=img, bg='white')
+imagen1.pack(padx= 150, pady= 150)
+#imagen1.grid(row=3, column=1)
+
+
 
 #Botones
 
 #Definir funciones back.end
-def click():
+def iniciar_transmision():
+	print("Iniciar")
 
-    lbl.configure(text="Click realizado")
+def terminar_tranmision():
+	print("Terminar")
 
-#def leer_archivo():
+def ver_transmision():
+	print("Ver")
+
+def activar_deteccion():
+	print("Activar")
+
+def ver_registro(): #funcion para leer archivo
+	print("Ver")
+	path_archivo = '/home/gaby/Descargas/Proyecto2_Embebidos/register.txt'
 	#archivo = open("register.txt", “r”)
+	#archivo = open(file=path_archivo)
+
+def importar_registro(): #funcion para leer archivo
+	print("Importar")
+
+def salir(): #funcion para cerrar app
+	exit()
 
 #Definir botones
 #Botón 1
-btn1 = Button(window, text="Empezar tranmisión", bg="black", fg="white", command=click)
-
-btn1.grid(column=0, row=10)
+btn1 = Button(window, text="Iniciar tranmisión", font=20, padx=10, pady=15, bg="white", fg="black", command=iniciar_transmision)
+btn1.place(x=60, y=100)
 
 #Botón 2
-btn2 = Button(window, text="Ver tranmisión", bg="black", fg="white", command=click)
-
-btn2.grid(column=0, row=15)
+btn2 = Button(window, text="Terminar tranmisión", font=20, padx=10, pady=15, bg="white", fg="black", command=terminar_tranmision)
+btn2.place(x=580, y=100)
 
 #Botón 3
-btn3 = Button(window, text="Terminar tranmisión", bg="black", fg="white", command=click)
-
-btn3.grid(column=0, row=20)
+btn3 = Button(window, text="Ver tranmisión", font=20, padx=10, pady=15, bg="white", fg="black", command=ver_transmision)
+btn3.place(x=60, y=300)
 
 #Botón 4
-btn4 = Button(window, text="Activar detección", bg="black", fg="white", command=click)
-
-btn4.grid(column=0, row=25)
+btn4 = Button(window, text="Activar detección", font=20, padx=10, pady=15, bg="white", fg="black", command=activar_deteccion)
+btn4.place(x=580, y=300)
 
 #Botón 5
-btn5 = Button(window, text="Importar registro", bg="black", fg="white", command=click)
-
-btn5.grid(column=0, row=30)
+btn5 = Button(window, text="Ver registro", font=20, padx=10, pady=15, bg="white", fg="black", command=ver_registro)
+btn5.place(x=60, y=500)
 
 #Botón 6
-btn6 = Button(window, text="Ver registro", bg="black", fg="white", command=click)
-
-btn6.grid(column=0, row=35)
+btn6 = Button(window, text="Importar registro", font=20, padx=10, pady=15, bg="white", fg="black", command=importar_registro)
+btn6.place(x=580, y=500)
 
 #Botón 7
-btn7 = Button(window, text="Salir", bg="black", fg="white", command=click)
-
-btn7.grid(column=0, row=40)
-
+btn7 = Button(window, text="Salir", font=30, padx=10, pady=15, bg="white", fg="black", command=salir)
+btn7.place(x=360, y=600)
 
 window.mainloop()
